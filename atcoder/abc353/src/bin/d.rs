26dacc526adb13816525118af513ca3d24d87ad8
mokoshi <r.koshi.0514@gmail.com>
2024-06-24 17:28:16 +0900

    353

--- atcoder/abc353/src/bin/d.rs
+++ atcoder/abc353/src/bin/d.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u64; n]
+    }
+    let mut ds = vec![0; n];
+    let mut ss = vec![0; n];
+    for i in 1..n {
+        ds[i] = ds[i - 1] + 10_u64.pow((a[i] as f64).log10() as u32 + 1);
+        ss[i] = ss[i - 1] + a[i];
+    }
+    // println!("{:?}", ds);
+    // println!("{:?}", ss);
+    let div = 998244353;
+    let mut ans = 0;
+    for i in 0..n - 1 {
+        ans += a[i] * ((ds[n - 1] - ds[i]) % div) + (ss[n - 1] - ss[i]);
+        ans %= div;
+    }
+    println!("{}", ans);
 }
--- atcoder/abc353/src/bin/e.rs
+++ atcoder/abc353/src/bin/e.rs
@@ -1,3 +1,65 @@
+// TODO トライ木実装 ing
+
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
+
+use std::fmt;
+
+struct TrieTree {
+    root: Node,
+}
+
+impl TrieTree {
+    fn new() -> Self {
+        Self { root: Node::new() }
+    }
+    fn add(&mut self, s: &Vec<char>) {
+        let mut n = &mut self.root;
+        for &c in s {
+            let idx = (c as u8 - b'a') as usize;
+            if let Some(next) = &n.v[idx] {
+                n = next;
+            }
+            // match &mut n.v[idx] {
+            //     Some(next) => n = next,
+            //     None => n.v[idx] = Some(Node::new()),
+            // }
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Node {
+    v: Vec<Option<Node>>,
+}
+
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Node {{ v: {:?} }}", self.v)
+    }
+}
+impl Node {
+    fn new() -> Self {
+        Self { v: vec![None; 26] }
+    }
+}
+
 fn main() {
-    todo!();
+    input! {
+        n :usize,
+        mut s: [Chars; n]
+    }
+    s.sort();
+    println!("{:?}", s);
+
+    let mut node = Node::new();
+    node.v[0] = None;
+    println!("{:?}", node);
+    // let mut q = VecDeque::from([0]);
+    // let mut e = vec![false; n];
+    // while !q.is_empty() {
+    //     let a = q.pop_front().unwrap();
+    //     let c = s[a][0];
+    // }
 }
